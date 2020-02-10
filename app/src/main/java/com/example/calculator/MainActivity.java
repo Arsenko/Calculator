428a842a3a45762f05f49e0bd9fc25e60b2200cf
Arsenko <minsenka@gmail.com>
2020-02-10 16:59:41 +0300

    final

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,48 +29,46 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
     public void init(){
-        View.OnClickListener universalListener= new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView tempBut=(TextView) v;
-                outputText.setText(outputText.getText().toString()+tempBut.getText().toString());
-            }
-        };
 
-        but1=findViewById(R.id.but1);
-        but1.setOnClickListener(universalListener);
-
-        but2=findViewById(R.id.but2);
-        but2.setOnClickListener(universalListener);
-
-        but3=findViewById(R.id.but3);
-        but3.setOnClickListener(universalListener);
-
-        but4=findViewById(R.id.but4);
-        but4.setOnClickListener(universalListener);
-
-        but5=findViewById(R.id.but5);
-        but5.setOnClickListener(universalListener);
-
-        but6=findViewById(R.id.but6);
-        but6.setOnClickListener(universalListener);
-
-        but7=findViewById(R.id.but7);
-        but7.setOnClickListener(universalListener);
-
-        but8=findViewById(R.id.but8);
-        but8.setOnClickListener(universalListener);
-
-        but9=findViewById(R.id.but9);
-        but9.setOnClickListener(universalListener);
-
-        but0=findViewById(R.id.but0);
-        but0.setOnClickListener(universalListener);
-
-        butDecPoint=findViewById(R.id.but0);
-        butDecPoint.setOnClickListener(universalListener);
+        outputText=findViewById(R.id.outputText);
+//        but1=findViewById(R.id.but1);
+//        but1.setOnClickListener(universalListener);
+//
+//        but2=findViewById(R.id.but2);
+//        but2.setOnClickListener(universalListener);
+//
+//        but3=findViewById(R.id.but3);
+//        but3.setOnClickListener(universalListener);
+//
+//        but4=findViewById(R.id.but4);
+//        but4.setOnClickListener(universalListener);
+//
+//        but5=findViewById(R.id.but5);
+//        but5.setOnClickListener(universalListener);
+//
+//        but6=findViewById(R.id.but6);
+//        but6.setOnClickListener(universalListener);
+//
+//        but7=findViewById(R.id.but7);
+//        but7.setOnClickListener(universalListener);
+//
+//        but8=findViewById(R.id.but8);
+//        but8.setOnClickListener(universalListener);
+//
+//        but9=findViewById(R.id.but9);
+//        but9.setOnClickListener(universalListener);
+//
+//        but0=findViewById(R.id.but0);
+//        but0.setOnClickListener(universalListener);
+//
+//        butDecPoint=findViewById(R.id.but0);
+//        butDecPoint.setOnClickListener(universalListener);
 
 
+    }
+    public void btnPress(View view) {
+        Button btn = (Button) view;
+        outputText.append(btn.getText());
     }
 
 
